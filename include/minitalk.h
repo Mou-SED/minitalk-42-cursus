46230ba3c1fab2c069bcd9a03a612b5a44947e40
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-11 17:41:39 +0100

    add a header file

---
+++ include/minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 16:20:37 by moseddik          #+#    #+#             */
+/*   Updated: 2022/05/11 17:08:59 by moseddik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+# include <stdlib.h>
+
+void	print_hello_client(void);
+void	print_hello_server(void);
+
+# endif
