3e8f7e8f87d28f1f2c2b700a8df5c5562e612afe
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-12 20:51:45 +0100

    test signals handler

--- server.c
+++ server.c
@@ -6,15 +6,18 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:19:39 by moseddik          #+#    #+#             */
-/*   Updated: 2022/05/12 14:47:44 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/05/12 20:49:56 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handle_siguser1(int signum)
+void	handle_siguser(int signum)
 {
-	printf("Signal %d recived.\n", signum);
+	if (signum == SIGZERO)
+		ft_putendl_fd("0", 1);
+	else if (signum == SIGONE)
+		ft_putendl_fd("1", 1);
 }
 
 int	main(void)
@@ -23,8 +26,8 @@ int	main(void)
 
 	pid = getpid();
 	printf("PID : %d\n", pid);
-	signal(SIGONE, handle_siguser1);
-	signal(SIGZERO, handle_siguser1);
+	signal(SIGZERO, handle_siguser);
+	signal(SIGONE, handle_siguser);
 	while (1)
 		pause();
 }
