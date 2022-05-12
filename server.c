498af23e3f464017b166585b5577f99181e410b9
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-12 23:28:30 +0100

    change signal to sigaction func

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:19:39 by moseddik          #+#    #+#             */
-/*   Updated: 2022/05/12 20:49:56 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/05/12 22:37:29 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	handle_siguser(int signum)
 {
+	printf("hi im here!");
 	if (signum == SIGZERO)
 		ft_putendl_fd("0", 1);
 	else if (signum == SIGONE)
@@ -22,12 +23,16 @@ void	handle_siguser(int signum)
 
 int	main(void)
 {
+	struct sigaction action;
 	pid_t	pid;
 
 	pid = getpid();
 	printf("PID : %d\n", pid);
-	signal(SIGZERO, handle_siguser);
-	signal(SIGONE, handle_siguser);
-	while (1)
+	action.sa_handler = handle_siguser;
+	sigemptyset(&action.sa_mask);
+	action.sa_flags = SA_RESTART;
+	sigaction(SIGONE, &action, NULL);
+	sigaction(SIGZERO, &action, NULL);
+	while (KEEP_GOING)
 		pause();
 }
