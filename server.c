78eea0b00ef7a1e93510246f1ebd8bcf67538428
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-12 11:08:15 +0100

    get and print pid of server in the terminal

--- server.c
+++ server.c
@@ -6,13 +6,15 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:19:39 by moseddik          #+#    #+#             */
-/*   Updated: 2022/05/11 17:10:38 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/05/12 10:24:03 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main (void)
+int	main(void)
 {
-	print_hello_server();
+	printf("%d\n", getpid());
+	while (1)
+		pause();
 }
