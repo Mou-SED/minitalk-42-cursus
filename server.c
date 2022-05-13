9f779b960c33c96d3d2ed734dbae9a92b697ccfc
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-13 17:55:52 +0100

    change handler file && add signals to mask

--- server.c
+++ server.c
@@ -6,31 +6,24 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:19:39 by moseddik          #+#    #+#             */
-/*   Updated: 2022/05/12 22:37:29 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/05/13 16:21:06 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	handle_siguser(int signum)
-{
-	printf("hi im here!");
-	if (signum == SIGZERO)
-		ft_putendl_fd("0", 1);
-	else if (signum == SIGONE)
-		ft_putendl_fd("1", 1);
-}
-
 int	main(void)
 {
-	struct sigaction action;
-	pid_t	pid;
+	struct sigaction	action;
+	pid_t				pid;
 
 	pid = getpid();
-	printf("PID : %d\n", pid);
-	action.sa_handler = handle_siguser;
+	ft_printf("PID : %d\n", pid);
 	sigemptyset(&action.sa_mask);
-	action.sa_flags = SA_RESTART;
+	sigaddset(&action.sa_mask, SIGZERO);
+	sigaddset(&action.sa_mask, SIGONE);
+	action.sa_flags = SA_SIGINFO;
+	action.sa_sigaction = handle_siguser;
 	sigaction(SIGONE, &action, NULL);
 	sigaction(SIGZERO, &action, NULL);
 	while (KEEP_GOING)
