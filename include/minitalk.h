21fe246cc17065cdfc214233724db0c143dedd1f
Mou-SED <moussa.seddik.ph@gmail.com>
2022-05-15 20:10:53 +0100

    handle ctrl + c in Unicode

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 16:20:37 by moseddik          #+#    #+#             */
-/*   Updated: 2022/05/14 07:49:06 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/05/15 20:00:01 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,20 @@
 # define RED "\e[1;31m"
 # define NC "\e[0m"
 
+typedef struct s_unicode
+{
+	pid_t	pid_client;
+	int		i_bit;
+	char	byte;
+	int		i_byte;
+	int		len_of_uni;
+}	t_unicode;
+
 int		check_get_pid(char *pid_str);
 void	ft_putendl_fd(char *s, int fd);
 int		send_message(char *message, pid_t pid);
 void	handle_siguser(int signum, siginfo_t *info, void *unused);
 void	confirm_message(int signum);
+void	ft_bzero(void *s, size_t n);
 
 #endif
--- src/server/receive_message.c
+++ src/server/receive_message.c
@@ -6,39 +6,87 @@
 /*   By: moseddik <moseddik@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 09:24:13 by moseddik          #+#    #+#             */
-/*   Updated: 2022/05/13 18:13:43 by moseddik         ###   ########.fr       */
+/*   Updated: 2022/05/15 20:08:15 by moseddik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-char	g_message;
+char	*g_message;
+
+static int	len_of_unicode(char first_byt)
+{
+	int	i;
+	int	shifted;
+	int	len;
+
+	shifted = first_byt >> 7;
+	if (shifted == 0)
+		return (1);
+	len = 0;
+	shifted = (first_byt >> 7) & 1;
+	i = 6;
+	while (shifted == 1 && i >= 0)
+	{
+		++len;
+		shifted = (first_byt >> i) & 1;
+		--i;
+	}
+	return (len);
+}
+
+static void	ft_diff_client(t_unicode *data, siginfo_t *info)
+{
+	ft_bzero(data, sizeof(t_unicode));
+	if (g_message != NULL)
+		free(g_message);
+	g_message = NULL;
+	data->pid_client = info->si_pid;
+}
+
+static void	ft_print_unicode(t_unicode *data)
+{
+	g_message[data->i_byte] = '\0';
+	ft_printf("%s", g_message);
+	data->i_byte = 0;
+	data->len_of_uni = 0;
+	if (g_message != NULL)
+		free(g_message);
+	g_message = NULL;
+}
+
+static void	allocate_unicode(t_unicode *data)
+{
+	data->len_of_uni = len_of_unicode(data->byte);
+	g_message = malloc(sizeof(char) * (data->len_of_uni + 1));
+	if (g_message == NULL)
+		exit(1);
+}
 
 void	handle_siguser(int signum, siginfo_t *info, void *unused)
 {
-	static int		i;
-	static pid_t	pid_client;
+	static t_unicode	data;
 
-	(void) unused;
-	if (pid_client == 0)
-		pid_client = info->si_pid;
-	else if (info->si_pid != pid_client)
+	(void)unused;
+	if (data.pid_client == 0)
+		data.pid_client = info->si_pid;
+	else if (info->si_pid != data.pid_client)
+		ft_diff_client(&data, info);
+	if (signum == SIGONE)
+		data.byte = (data.byte << 1) | 1;
+	else if (signum == SIGZERO)
+		data.byte = (data.byte << 1);
+	data.i_bit++;
+	if (data.i_bit == 8 && data.byte == '\0')
+		kill(data.pid_client, SIGONE);
+	else if (data.i_bit == 8)
 	{
-		pid_client = info->si_pid;
-		g_message = 0;
-		i = 0;
-	}
-	if (signum == SIGZERO)
-		g_message <<= 1;
-	else if (signum == SIGONE)
-		g_message = (g_message << 1) | 1;
-	i++;
-	if (i == 8)
-	{
-		if (g_message == '\0')
-			kill(pid_client, SIGONE);
-		ft_printf("%c", g_message);
-		g_message = 0;
-		i = 0;
+		if (data.len_of_uni == 0)
+			allocate_unicode(&data);
+		g_message[data.i_byte++] = data.byte;
+		if (data.i_byte == data.len_of_uni)
+			ft_print_unicode(&data);
+		data.byte = 0;
+		data.i_bit = 0;
 	}
 }
